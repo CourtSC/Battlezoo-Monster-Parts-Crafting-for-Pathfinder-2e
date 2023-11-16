93adeb6b631bc328eef39cc8683987c7fce0b912
CourtSC <scott.c.court@gmail.com>
2023-11-16 18:36:26 -0500

    Created class for the imbuements sheet

--- scripts/monsterParts.js
+++ scripts/monsterParts.js
@@ -1,3 +1,11 @@
+class imbuementsSheet {
+	static ID = 'Battlezoo-Monster-Parts-Crafting-for-Pathfinder-2e';
+
+	static IMBUEMENTS = 'imbuements';
+
+	static TEMPLATES = `modules/${this.ID}/templates/monsterParts.hbs`;
+}
+
 Hooks.on('renderItemSheetPF2e', (itemSheet, html) => {
 	// find the element which has the item sheet tabs
 	const itemSheetTabs = html.find('[class="sheet-tabs tabs"]');
